16d12bd0c5e145ec19ebbf7b99983ab200d23949
Tomáš Ďuriš <xduris05@vutbr.cz>
2019-10-18 15:17:36 +0200

    INP functional project finally

--- 2. ročník/INP/1. projekt/fpga/ledc8x8.vhd
+++ 2. ročník/INP/1. projekt/fpga/ledc8x8.vhd
@@ -19,11 +19,10 @@ architecture main of ledc8x8 is
 	signal rows : std_logic_vector(0 to 7) := "10000000";
 	signal leds : std_logic_vector(0 to 7) := (others => '1');
 	signal rise : std_logic := '0';
-	signal switch_state : std_logic := '0';
-	signal counter : std_logic_vector(0 to 7) := (others => '0');
-	signal counter2 : std_logic_vector(0 to 23) := (others => '0');
-	signal dont_switch : std_logic_vector(0 to 1) := "00";
-	signal state: std_logic_vector(0 to 1) := "00";
+	signal switch_state : std_logic_vector(0 to 1) := "00";
+	signal counter : std_logic_vector(11 downto 0) := (others => '0');
+	signal counter2 : std_logic_vector(23 downto 0) := (others => '0');
+	signal state : std_logic := '0';
 	
 begin
 
@@ -32,30 +31,42 @@ begin
 		if(RESET = '1') then
 			counter <= (others => '0');
 		elsif (rising_edge(SMCLK)) then
-			counter <= counter + 1;
-			counter2 <= counter2 + 1;
-		end if;
-		
-		if (counter2 = "1110000100000000000000") then
-			switch_state <= '1';
-		elsif (counter2 = "11100001000000000000000") then
-			switch_state <= '1';
-		elsif (counter2 = "111000010000000000000000") then
-			switch_state <= '1';
-			dont_switch <= "01";
-		else 
-			switch_state <= '0';
+			if switch_state = "00" or switch_state = "01" then
+				if switch_state /= "10" then
+					counter2 <= counter2 + 1;
+				end if;
+			end if;
+			
+			if (counter2 = 	 "1110000100000000000000") then
+				switch_state <= "01";
+			elsif (counter2 = "11100001000000000000000") then
+				switch_state <= "10";
+				counter2 <= (others => '0');
+			end if;
+			
+			if counter = "111000010000" then
+				counter <= (others => '0');
+				rise <= '1';
+			else
+				counter <= counter + 1;
+				rise <= '0';
+			end if;		
+
+			case switch_state is
+				when "00" => state <= '0';
+				when "01" => state <= '1';
+				when "10" => state <= '0';
+				when others => null;
+			end case;
 		end if;
 	end process;
-	rise <= '1' when counter = "11111111" else '0';
-	
-	
-process(RESET, SMCLK, counter)
+		
+process(RESET, SMCLK, rise)
     begin
         if RESET = '1' then
             rows <= "10000000";
         elsif rising_edge(SMCLK) and rise = '1' then
-			case rows is
+				case rows is
                 when "10000000" => rows <= "01000000";
                 when "01000000" => rows <= "00100000";
                 when "00100000" => rows <= "00010000";
@@ -67,21 +78,12 @@ process(RESET, SMCLK, counter)
                 when others => null;
             end case;
         end if;
-		  
-		  if(switch_state = '1') and (dont_switch /= "01") then
-
-					case state is
-						when "00" => state <= "11";
-						when "11" => state <= "00";
-						when others => state <= "00";
-					end case;
-			end if;
     end process;
 
 	
 	process(state, rows)
 	begin
-		if (state = "00") then
+		if (state = '0') then
 			case (rows) is				
 				when "10000000" =>
 					leds <= "00000111";
@@ -102,29 +104,6 @@ process(RESET, SMCLK, counter)
 				when others =>
 					leds <= "11111111";
 			end case;
-		
-		elsif (state = "10") then
-			case (rows)  is			
-				when "10000000" =>
-					leds <= "11111111";
-				when "01000000" =>
-					leds <= "11111111";
-				when "00100000" =>
-					leds <= "11111111";
-				when "00010000" =>
-					leds <= "11111111";
-				when "00001000" =>
-					leds <= "11111111";
-				when "00000100" =>
-					leds <= "11111111";
-				when "00000010" =>
-					leds <= "11111111";
-				when "00000001" =>
-					leds <= "11111111";
-				when others =>
-					leds <= "11111111";
-			end case;
-			
 		else 
 			leds <= "11111111";
 		end if;
